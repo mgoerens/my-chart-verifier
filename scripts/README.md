f8b0317fae17d1bdf7b520f425530912678786c2
mgoerens <mgoerens@redhat.com>
2023-08-02 16:37:08 +0200

    sync with origin/remove_docker_tests

--- scripts/README.md
+++ scripts/README.md
@@ -3,8 +3,6 @@
 
 - ```release/versionchecker.py```
     - used to determine if a PR contains a version update. 
--  ```buildandtest/buildandtest.py```
-    - used to build a docker image and then test created image.
 - ```report/report-info.py```
     - used to generate of report of a chart verifier verify report.
     - exact copy of same script from chart repo    
--- scripts/src/release/releasebody.py
+++ scripts/src/release/releasebody.py
@@ -1,13 +1,12 @@
 import sys
 
-sys.path.append("./scripts/src/")
-from release import releasechecker  # noqa E402
-
+sys.path.append('./scripts/src/')
+from release import releasechecker
+from utils import utils
 
 def get_release_body(version, image_name, release_info):
     """Generate the body of the GitHub release"""
-    body = f"Chart verifier version {version} <br><br>"
-    body += f"Docker Image:<br>- {image_name}:{version}<br><br>"
+    body = f"Chart verifier version {version} <br><br>Docker Image:<br>- {image_name}:{version}<br><br>"
     body += "This version includes:<br>"
     for info in release_info:
         if info.startswith("<"):
@@ -16,12 +15,7 @@ def get_release_body(version, image_name, release_info):
             body += f"- {info}<br>"
     return body
 
-
 def main():
     version_info = releasechecker.get_version_info()
-    release_body = get_release_body(
-        version_info["version"],
-        version_info["quay-image"],
-        version_info["release-info"],
-    )
+    release_body = get_release_body(version_info["version"],version_info["quay-image"],version_info["release-info"])
     print(release_body)
